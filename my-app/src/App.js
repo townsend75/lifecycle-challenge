f1a18284e9ca9bf126f69d578860bd0db3a79150
townsend75 Townsend <dtownsend@gmx.net>
2023-11-10 12:02:45 +0000

    add loader.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <Content />
     </div>
   );
 }
